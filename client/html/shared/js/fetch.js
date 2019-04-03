5c867e4f33df894e151d23e6d7d315977e43ff0e
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-03 12:19:52 -0300

    Fix customFetch

--- client/html/shared/js/fetch.js
+++ client/html/shared/js/fetch.js
@@ -1,6 +1,6 @@
 const customFetch = (url = "", options = {}) => {
   options.credentials = "include";
-  const _url = `${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/api/`;
+  const _url = `${process.env.SERVER_HOST}:${process.env.SERVER_PORT}${url}`;
   return fetch(_url, options);
 };
 
