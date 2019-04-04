9baa6d4903d5c72323b27a3ab823146bc74b2f06
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-04 00:27:18 -0300

    Add basic login functionality

--- client/html/login/script.js
+++ client/html/login/script.js
@@ -1,35 +1,35 @@
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
 import { sha256 } from "js-sha256";
 import customFetch from "../shared/js/fetch";
+import { encodeObj } from "../shared/js/utils";
 
 window.onload = () => {
   document.querySelector("#login-form").addEventListener("submit", event => {
     event.preventDefault();
     const username = document.querySelector("#username").value;
     const password = sha256(document.querySelector("#password").value);
-    console.log({ username, password });
+    //console.log({ username, password });
     customFetch("/api/login.php/", {
       method: "post",
       headers: {
         "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
       },
-      body: `username=${username}&password=${password}`
+      body: encodeObj({ username, password })
     })
       .then(res => {
         if (res.status !== 200) {
-          throw new Error("Qué problema che :/");
+          throw new Error(res.statusText);
         }
         return res.json();
       })
       .then(msg => {
+        document.querySelector("#error-msg").classList.remove("scale-in");
+        document.location = "/home/";
         console.log({ msg });
-        setTimeout(
-          function() {
-            window.location = "/";
-          }.bind(this),
-          2000
-        );
       })
       .catch(err => {
+        document.querySelector("#error-msg").classList.add("scale-in");
         console.error({ err });
       });
   });
