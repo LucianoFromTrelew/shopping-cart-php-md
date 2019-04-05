4845713c92b2189e7a8ae357cb8d7d903098e38f
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-05 17:12:01 -0300

    Style login form

--- client/html/login/script.js
+++ client/html/login/script.js
@@ -7,30 +7,36 @@ import { encodeObj } from "../shared/js/utils";
 window.onload = () => {
   document.querySelector("#login-form").addEventListener("submit", event => {
     event.preventDefault();
+
     const username = document.querySelector("#username").value;
     const password = sha256(document.querySelector("#password").value);
-    //console.log({ username, password });
-    customFetch("/api/login.php/", {
-      method: "post",
-      headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-      body: encodeObj({ username, password })
-    })
-      .then(res => {
-        if (res.status !== 200) {
-          throw new Error(res.statusText);
-        }
-        return res.json();
+
+    document.querySelector("#spinner").classList.add("scale-in");
+
+    setTimeout(() => {
+      customFetch("/api/login.php/", {
+        method: "post",
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        body: encodeObj({ username, password })
       })
-      .then(msg => {
-        document.querySelector("#error-msg").classList.remove("scale-in");
-        document.location = "/home/";
-        console.log({ msg });
-      })
-      .catch(err => {
-        document.querySelector("#error-msg").classList.add("scale-in");
-        console.error({ err });
-      });
+        .then(res => {
+          if (res.status !== 200) {
+            throw new Error(res.statusText);
+          }
+          return res.json();
+        })
+        .then(msg => {
+          document.querySelector("#error-msg").classList.remove("scale-in");
+          document.location = "/home/";
+        })
+        .catch(err => {
+          document.querySelector("#error-msg").classList.add("scale-in");
+        })
+        .finally(() => {
+          document.querySelector("#spinner").classList.remove("scale-in");
+        });
+    }, 2000);
   });
 };
