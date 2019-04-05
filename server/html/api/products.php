8f9262cd3f4d96026fc4c9f932457adebb4e4e90
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-05 12:57:06 -0300

    Add products endpoint

---
+++ server/html/api/products.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once("_init.php");
+require_once(INCLUDES_PATH . "db.php");
+
+session_start();
+
+$link = get_connection();
+
+if ($result = $link->query("SELECT * FROM products")) {
+
+    $json = array();
+    $index = 0;
+
+    while ($row = $result->fetch_assoc()) {
+        $json[$index] = array(
+            "id" => $row["id"],
+            "name" => $row["name"],
+            "stock" => $row["stock"],
+            "price" => $row["price"],
+            "description" => $row["description"],
+            "imageUrl" => $row["imageUrl"],
+        );
+        $index++;
+    }
+
+    http_response_code(200);
+    echo json_encode($json);
+
+} else {
+
+    http_response_code(500);
+    echo json_encode(array(
+        "msg" => "Something went wrong :/"
+    ));
+
+}
+
+
+?>
