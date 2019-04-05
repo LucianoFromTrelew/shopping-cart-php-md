b003fc1333fd8c2735e13dcf07c9af79513fd9d3
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-05 16:34:32 -0300

    Check if user is logged in on index.php

--- server/html/api/index.php
+++ server/html/api/index.php
@@ -1,15 +1,19 @@
 <?php
+
+require_once("_init.php");
+require_once(INCLUDES_PATH . "auht.php");
+
 session_start();
 
 
-if(isset($_SESSION['user'])) {
-  error_log("USUARIO LOGUEADO");
+if(is_logged_in()) {
+    error_log("USUARIO LOGUEADO");
+    http_response_code(200);
+    echo json_encode(array("msg" => "Hurray! You are logged in :D"));
 } else {
-  error_log("USUARIO NO ESTA LOGUEADO");
+    error_log("USUARIO NO ESTA LOGUEADO");
+    http_response_code(401);
+    echo json_encode(array("msg" => "You are not logged in :/"));
 }
 
-error_log(print_r(apache_request_headers(), TRUE));
-error_log(print_r("Session counter => ".$_SESSION['counter'], TRUE));
-
-echo json_encode(array("msg" => $_SESSION['counter']));
 ?>
