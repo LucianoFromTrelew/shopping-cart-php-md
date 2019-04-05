13ccbe9e123856dd2df50cb26379a6acf483e0db
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-05 16:34:40 -0300

    Check if user is logged in on products.php

--- server/html/api/products.php
+++ server/html/api/products.php
@@ -2,36 +2,46 @@
 
 require_once("_init.php");
 require_once(INCLUDES_PATH . "db.php");
+require_once(INCLUDES_PATH . "auth.php");
 
 session_start();
 
-$link = get_connection();
+if (is_logged_in()) {
+    $link = get_connection();
 
-if ($result = $link->query("SELECT * FROM products")) {
+    if ($result = $link->query("SELECT * FROM products")) {
 
-    $json = array();
-    $index = 0;
+        $json = array();
+        $index = 0;
 
-    while ($row = $result->fetch_assoc()) {
-        $json[$index] = array(
-            "id" => $row["id"],
-            "name" => $row["name"],
-            "stock" => $row["stock"],
-            "price" => $row["price"],
-            "description" => $row["description"],
-            "imageUrl" => $row["imageUrl"],
-        );
-        $index++;
+        while ($row = $result->fetch_assoc()) {
+            $json[$index] = array(
+                "id" => $row["id"],
+                "name" => $row["name"],
+                "stock" => $row["stock"],
+                "price" => $row["price"],
+                "description" => $row["description"],
+                "imageUrl" => $row["imageUrl"],
+            );
+            $index++;
+        }
+
+        http_response_code(200);
+        echo json_encode($json);
+
+    } else {
+
+        http_response_code(500);
+        echo json_encode(array(
+            "msg" => "Something went wrong :/"
+        ));
+
     }
-
-    http_response_code(200);
-    echo json_encode($json);
-
 } else {
 
-    http_response_code(500);
+    http_response_code(401);
     echo json_encode(array(
-        "msg" => "Something went wrong :/"
+        "msg" => "You are not logged in :/"
     ));
 
 }
