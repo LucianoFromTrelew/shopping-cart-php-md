abe570589de4a36b68e0a80e1ab93f0715133cd2
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-05 12:59:14 -0300

    Check if logged in on index

--- server/html/api/index.php
+++ server/html/api/index.php
@@ -1,14 +1,15 @@
 <?php
-  session_start();
-  
-  if(isset($_SESSION['user'])) {
-    error_log("USUARIO LOGUEADO");
-  } else {
-    error_log("USUARIO NO ESTA LOGUEADO");
-  }
+session_start();
 
-  error_log(print_r(apache_request_headers(), TRUE));
-  error_log(print_r("Session counter => ".$_SESSION['counter'], TRUE));
 
-  echo json_encode(array("msg" => $_SESSION['counter']));
+if(isset($_SESSION['user'])) {
+  error_log("USUARIO LOGUEADO");
+} else {
+  error_log("USUARIO NO ESTA LOGUEADO");
+}
+
+error_log(print_r(apache_request_headers(), TRUE));
+error_log(print_r("Session counter => ".$_SESSION['counter'], TRUE));
+
+echo json_encode(array("msg" => $_SESSION['counter']));
 ?>
