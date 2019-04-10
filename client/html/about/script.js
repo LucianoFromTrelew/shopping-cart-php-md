da2ed73ad9bd2f455393ef7c221eef5640ef089d
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-10 08:22:49 -0300

    Fix about page imports

--- client/html/about/script.js
+++ client/html/about/script.js
@@ -1,24 +1,24 @@
-import customFetch from "../shared/js/fetch";
+import customFetch from '/static/js/fetch';
 
 window.onload = () => {
   const urlParam = new URLSearchParams(window.location.search);
 
-  console.log(urlParam.has("post"));
-  console.log(urlParam.get("post"));
+  console.log(urlParam.has('post'));
+  console.log(urlParam.get('post'));
 
-  const postDiv = document.querySelector("#post");
-  if (urlParam.has("post") && urlParam.get("post")) {
-    postDiv.innerHTML = `Post number ${urlParam.get("post")}`;
+  const postDiv = document.querySelector('#post');
+  if (urlParam.has('post') && urlParam.get('post')) {
+    postDiv.innerHTML = `Post number ${urlParam.get('post')}`;
   } else {
-    postDiv.innerHTML = "No post provided";
+    postDiv.innerHTML = 'No post provided';
   }
 
-  customFetch("/api/")
+  customFetch('/api/')
     .then(res => res.json())
     .then(msg => {
-      console.log({ ...msg });
+      console.log({...msg});
     })
     .catch(err => {
-      console.error({ err });
+      console.error({err});
     });
 };
