678d06e4f2d3ce340224cc94579cc117465a06d3
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-10 14:35:42 -0300

    Echo  on index.php

--- server/html/api/index.php
+++ server/html/api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("_init.php");
-require_once(INCLUDES_PATH . "auht.php");
+require_once(INCLUDES_PATH . "auth.php");
 
 session_start();
 
@@ -9,7 +9,7 @@ session_start();
 if(is_logged_in()) {
     error_log("USUARIO LOGUEADO");
     http_response_code(200);
-    echo json_encode(array("msg" => "Hurray! You are logged in :D"));
+    echo json_encode($_SESSION);
 } else {
     error_log("USUARIO NO ESTA LOGUEADO");
     http_response_code(401);
