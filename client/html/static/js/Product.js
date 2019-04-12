b92849b3c37873e25f553bfa51b70f4ccbf8c158
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2019-04-12 07:21:41 -0300

    Make Product component abstract

--- client/html/static/js/Product.js
+++ client/html/static/js/Product.js
@@ -131,55 +131,15 @@ export default class Product {
     return '';
   }
   markup() {
-    return `
-            <div id="product-component" class="col xl4 l6 m12 s12">
-              <div class="card ${this.isOutOfStock()}">
-                <div class="card-image">
-                  <img src="${this.product['imageUrl']}" />
-                  <span class="card-title pink lighten-2">${
-                    this.product['name']
-                  }</span>
-                </div>
-                <div class="card-content">
-                  <span class="truncate">${this.product['description']}</span>
-                </div>
-                <div class="card-action">
-                  <b><span id="price" class="orange-text">$${
-                    this.product['price']
-                  }</span></b>
-                  <span class="">Stock: <b id="stock">${
-                    this.getStock() === 0 ? 'OUT' : this.getStock()
-                  }</b></span>
-                  <div>
-                    <a id="add-btn" class="btn-floating waves-effect waves-light red ${this.isOutOfStock()}"
-                      ><i class="material-icons">add</i></a
-                    >
-                    <a id="sub-btn" class="btn-floating waves-effect waves-light red"
-                      ><i class="material-icons">remove</i></a
-                    >
-                  </div>
-                </div>
-              </div>
-            </div>
-    `;
+    throw new TypeError(
+      'Abstract class "Product" cannot be instantiated directly',
+    );
   }
   constructor(container, product) {
-    /*
-     * container => element container
-     * product => product to display
-     * emitts `product-added` event on add button click
-     * emitts `product-substracted` event on sub button click
-     * */ // The constructor should only contain the boiler plate code for finding or creating the reference.
-    if (typeof container.dataset.ref === 'undefined') {
-      this.ref = uuid();
-      this.product = product;
-      Product.refs[this.ref] = this;
-      container.dataset.ref = this.ref;
-      this.init(container);
-    } else {
-      // If this element has already been instantiated, use the existing reference.
-      return Product.refs[container.dataset.ref];
+    if (this.constructor === Product) {
+      throw new TypeError(
+        'Abstract class "Product" cannot be instantiated directly',
+      );
     }
   }
 }
-Product.refs = {};
